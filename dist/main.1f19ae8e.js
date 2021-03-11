e31f84dcfc5ac18a00f33a04fa6a8939923eeace
Lionyu <779625444@qq.com>
2021-03-11 15:43:45 +0800

    init

--- dist/main.1f19ae8e.js
+++ dist/main.1f19ae8e.js
@@ -118,35 +118,7 @@ parcelRequire = (function (modules, cache, entry, globalName) {
 
   return newRequire;
 })({"main.js":[function(require,module,exports) {
-// let html = document.querySelector('#html')
-// let style = document.querySelector('#style')
-// let string = `sadsadasdasdasdsadsadasdsa`
-// let string2 = "";
-// console.log(string.length);
-// let n = 0;
-// let step = () => {
-//     setTimeout(() => {
-//         //如果是回车,就不照搬
-//         //如果不是回车,就照搬
-//         if (string[n] === "\n") {
-//             string2 += "<br>";
-//         } else if (string[n] === " ") {
-//             string2 += "&nbsp;";
-//         } else {
-//             string2 += string[n];
-//         }
-//         html.innerHTML = string2;
-//         style.innerHTML = string.substring(0, n)
-//         window.scrollTo(0, 99999)
-//         html.scrollTo(0, 99999)
-//         if (n + 1 < string.length) {
-//             //不是最后一个
-//             n += 1;//54
-//             step();
-//         }
-//     }, 0);
-// };
-// step()
+
 },{}],"../../../AppData/Local/Yarn/Data/global/node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
@@ -175,7 +147,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "55847" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "56782" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
